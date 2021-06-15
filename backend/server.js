9d5fe0b2a4f4ea9f070146fb23db965e58e4f8b0
tomryb <1rybak.tomasz@gmail.com>
2021-06-15 21:47:41 +0200

    test2

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const coursesRoutes = require('./routes/courses');
-const usersRoutes = require('./routes/users');
+const coursesRoutes = require("./routes/courses");
+const usersRoutes = require("./routes/users");
 
 const server = express();
 const PORT = process.env.PORT || 5000;
@@ -11,10 +11,32 @@ const PORT = process.env.PORT || 5000;
 server.use(bodyParser.json());
 server.use(cors());
 
-server.use('/courses', coursesRoutes);
-server.use('/users', usersRoutes);
+server.use("/courses", coursesRoutes);
+server.use("/users", usersRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    // Serve any static files
+    server.use(express.static(path.join(__dirname, "frontend/build")));
+    // Handle React routing, return all requests to React app
+    server.get("*", function (req, res) {
+      res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+    });
+  }
+  const whitelist = ["http://localhost:3000", "http://localhost:5000"];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      console.log("** Origin of request " + origin);
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        console.log("Origin acceptable");
+        callback(null, true);
+      } else {
+        console.log("Origin rejected");
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
 
 // server.listen(8000, () => console.log('Server for course is started...'));
-server.listen(PORT, () => console.log('Server for course is started...'));
+server.listen(PORT, () => console.log("Server for course is started..."));
+
 
--- frontend/src/helpers/request.js
+++ frontend/src/helpers/request.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 const request = axios.create({
-  baseURL: API_URL,
+  baseURL: "/api",
   validateStatus: false
 });
 
