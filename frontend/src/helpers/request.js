140a02d67deb79c64dad6ae9e1aa1ec33da7427c
tomryb <1rybak.tomasz@gmail.com>
2021-06-15 19:00:48 +0200

    test

--- frontend/src/helpers/request.js
+++ frontend/src/helpers/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "748f33b9-dc7a-4b5e-bdce-5d6fc4c18993",
+  baseURL: "https://awesome-turing-7e67d2.netlify.app/",
   validateStatus: false
 });
 
